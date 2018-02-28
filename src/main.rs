161ae82e69903cf82b70c60aa63c5b5d57421360
Jamie Nicol <jamie@jamienicol.me>
2018-02-28 11:22:12 +0000

    Create a window with ggez

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+extern crate ggez;
+
+use ggez::{Context, GameResult};
+use ggez::conf::Conf;
+use ggez::event;
+use ggez::graphics;
+
+struct MainState {
+}
+
+impl MainState {
+    fn new(_ctx: &mut Context) -> GameResult<Self> {
+        let s = MainState {
+        };
+        Ok(s)
+    }
+}
+
+impl event::EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx);
+        Ok(())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut c = Conf::new();
+    c.window_setup.title = "Turn Based Football".to_string();
+    let mut ctx = Context::load_from_conf("tbf", "Jamie Nicol", c).unwrap();
+    let mut state = MainState::new(&mut ctx).unwrap();
+    event::run(&mut ctx, &mut state).unwrap();
 }
