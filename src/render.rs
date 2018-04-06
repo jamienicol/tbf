6b35dbfbbe137f165c67e9890abeed78998caf18
Jamie Nicol <jamie@jamienicol.me>
2018-04-06 16:40:40 +0100

    render tilemap

--- src/render.rs
+++ src/render.rs
@@ -58,53 +58,44 @@ where
     fn run(&mut self, data: Self::SystemData) {
         let (assets, map, positions, sizes, sprites) = data;
 
-        // let tileset = &map.map.tilesets[0];
-        // let tileset_image = assets.images[&tileset.name].clone();
-        // let mut spritebatch = SpriteBatch::new(tileset_image);
+        // render map
+        let tileset = &map.map.tilesets[0];
+        let tileset_texture = &assets.images[&tileset.name];
 
-        // let layer = &map.map.layers[0];
-        // for x in 0..map.map.width {
-        //     for y in 0..map.map.height {
-        //         let gid = layer.tiles[y as usize][x as usize];
+        let layer = &map.map.layers[0];
+        for x in 0..map.map.width {
+            for y in 0..map.map.height {
+                let gid = layer.tiles[y as usize][x as usize];
 
-        //         let tile_y = (gid - 1) / (tileset.images[0].width as u32 / tileset.tile_width);
-        //         let tile_x = (gid - 1) % (tileset.images[0].width as u32 / tileset.tile_width);
+                let tile_y = (gid - 1) / (tileset.images[0].width as u32 / tileset.tile_width);
+                let tile_x = (gid - 1) % (tileset.images[0].width as u32 / tileset.tile_width);
 
-        //         let src = Rect {
-        //             x: (tile_x * tileset.tile_width) as f32 / tileset.images[0].width as f32,
-        //             y: (tile_y * tileset.tile_height) as f32 / tileset.images[0].height as f32,
-        //             w: tileset.tile_width as f32 / tileset.images[0].width as f32,
-        //             h: tileset.tile_height as f32 / tileset.images[0].height as f32,
-        //         };
+                let mut sprite = sprite::Sprite::new();
 
-        //         let dest = Point2::new(
-        //             (x * map.map.tile_width) as f32,
-        //             (y * map.map.tile_height) as f32,
-        //         );
+                sprite.set_pos(
+                    (x * map.map.tile_width) as f32,
+                    (y * map.map.tile_height) as f32,
+                );
 
-        //         spritebatch.add(DrawParam {
-        //             src: src,
-        //             dest: dest,
-        //             ..Default::default()
-        //         });
-        //     }
-        // }
+                sprite.set_size(
+                    tileset.tile_width as f32,
+                    tileset.tile_height as f32
+                );
 
-        // spritebatch
-        //     .draw(&mut self.ctx, Point2::new(0.0, 0.0), 0.0)
-        //     .unwrap();
+                sprite.set_tex_rect(
+                    (tile_x * tileset.tile_width) as f32 / tileset.images[0].width as f32,
+                    (tile_y * tileset.tile_height) as f32 / tileset.images[0].height as f32,
+                    tileset.tile_width as f32 / tileset.images[0].width as f32,
+                    tileset.tile_height as f32 / tileset.images[0].height as f32,
+                );
 
-        // for (position, _size, sprite) in (&positions, &sizes, &sprites).join() {
-        //     let image = &assets.images[sprite.image_id];
-        //     image
-        //         .draw(
-        //             &mut self.ctx,
-        //             Point2::new(position.pos.x.round(), position.pos.y.round()),
-        //             0.0,
-        //         )
-        //         .unwrap();
-        // }
+                // TODO: batch these
+                self.sprite_renderer
+                    .render_sprite(self.encoder, &self.out, &sprite, tileset_texture);
+            }
+        }
 
+        // render sprite components
         for (position, size, sprite) in (&positions, &sizes, &sprites).join() {
             let texture = &assets.images[sprite.image_id];
 
